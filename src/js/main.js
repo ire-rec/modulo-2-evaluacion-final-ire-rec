c88be5e5cc4afa38bee58385bdd22d305e7901fa
ire-rec <i.recavarren@gmail.com>
2022-07-17 02:36:07 +0200

    add inner html

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,22 @@
 'use strict';
+// const textSearch = document.querySelector ('.js-textSearch"');
+// const button = document.querySelector ('.js-button');
+// const reset = document.querySelector ('.js-reset');
+const listUl = document.querySelector('.js-animeUl');
+let datas = [];
+
+function renderAnimes(){
+  let html ='';
+  for(const animeData of datas){
+    html += `<li>${animeData.title} <img src ="${animeData.images.jpg.image_url}"></li>`;
+  }
+  listUl.innerHTML = html;
+}
+fetch('https://api.jikan.moe/v4/anime')
+  .then(response => response.json())
+  .then((json) => {
+    console.log(json.data);
+    datas = json.data;
+
+    renderAnimes();
+  });
